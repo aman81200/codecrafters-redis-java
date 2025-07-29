e1e34576128ec66d904de81e3ca4ef9bd5455169
Aman Karamchandani <akaram8.ak@gmail.com>
2025-07-29 20:31:30 +0530

    Handling Multiple request at a time #3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,59 +1,55 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.ClientInfoStatus;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+  public static void main(String[] args) {
+      // You can use print statements as follows for debugging, they'll be visible when running tests.
+      System.out.println("Logs from your program will appear here!");
 
-      //Uncomment this block to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
+      int port = 6379;
+      boolean listening = true;
+
+      try (ServerSocket serverSocket = new ServerSocket(port)) {
+
           // Since the tester restarts your program quite often, setting SO_REUSEADDR
           // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
-          clientSocket = serverSocket.accept();
-          // Wait for connection from client.
-            ServerSocket finalServerSocket = serverSocket;
 
-            Thread t1 = new Thread(
-                    ()->{
-                        try {
-                            returnPong(clientSocket, finalServerSocket);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-            );
-            t1.start();
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
+          // Wait for connection from client.
+          while (listening) {
+              Socket clientSocket = serverSocket.accept();
+              System.out.println("New Client Connected");
+              new Thread(
+                      () -> {
+                          returnPong(clientSocket);
+                      }
+              ).start();
           }
-        }
+      } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+      }
   }
 
-  public static void returnPong(Socket clientSocket,ServerSocket serverSocket) throws IOException {
-      while(true){
-          byte[] input = new byte[1024];
-          clientSocket.getInputStream().read(input);
-          String inputString = new String(input).trim();
-          System.out.println("Got Input " + inputString);
-          if(!inputString.isEmpty()){
-              clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+  public static void returnPong(Socket clientSocket) {
+      try(clientSocket;
+          OutputStream outputStream = clientSocket.getOutputStream();
+          BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+      ) {
+          while (true) {
+              if(in.readLine() == null){
+                  break;
+              }
+
+              in.readLine();
+              String line = in.readLine();
+              System.out.println("Last Line "+ line);
+              outputStream.write("+PONG\r\n".getBytes());
+              System.out.println("Wrote Pong");
           }
+      } catch (Exception e) {
+          throw new RuntimeException(e);
       }
   }
 }
